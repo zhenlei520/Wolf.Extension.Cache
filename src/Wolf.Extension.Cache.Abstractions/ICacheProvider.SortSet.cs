792c3e45a21ee49b33ca8482bcc22f1ceba875c8
zhenlei520 <lei941007>
2021-02-04 00:21:57 +0800

    调整缓存配置

--- src/Wolf.Extension.Cache.Abstractions/ICacheProvider.SortSet.cs
+++ src/Wolf.Extension.Cache.Abstractions/ICacheProvider.SortSet.cs
@@ -10,106 +10,196 @@ namespace Wolf.Extension.Cache.Abstractions
     /// </summary>
     public partial interface ICacheProvider
     {
-         /// <summary>添加 (当score一样value一样时不插入)</summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
-        /// <param name="score"></param>
-        /// <param name="isOverlap"></param>
-        /// <returns></returns>
-        bool SortedSetAdd<T>(string key, T value, double score, bool isOverlap = false);
+        #region 同步
 
-        /// <summary>删除</summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
+        /// <summary>
+        /// 设置SortSet类型的缓存键值对
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">缓存值</param>
+        /// <param name="score">分值</param>
+        /// <returns></returns>
+        bool SortedSet(string key, string value, double score);
+
+        /// <summary>
+        /// 设置SortSet类型的缓存键值对
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">缓存值</param>
+        /// <param name="score">分值</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        bool SortedSet<T>(string key, T value, double score);
+
+        /// <summary>
+        /// 删除指定的缓存键的value
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">值</param>
+        /// <returns></returns>
+        bool SortedSetRemove(string key, string value);
+
+        /// <summary>
+        /// 删除指定的缓存键的value
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">值</param>
+        /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         bool SortedSetRemove<T>(string key, T value);
 
-        /// <summary>获取全部</summary>
-        /// <param name="key"></param>
-        /// <param name="count"></param>
+        /// <summary>
+        /// 根据排名查询指定缓存的count数量的值
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="count">数量</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
         /// <returns></returns>
-        List<string> SortedSetRangeByRank(string key, long count = 1000);
+        List<string> SortedSetRangeByRank(string key, long count = 1000, bool isDesc = true);
 
-        /// <summary>获取全部</summary>
-        /// <param name="key"></param>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        List<T> SortedSetRangeByRank<T>(string key, long count = 1000) where T : class, new();
-
-        /// <summary>获取已过期的hashKey</summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        List<(string, string, string, string)> SortedSetRangeByRankAndOverTime(long count = 1000);
-
-        /// <summary>降序获取指定索引的集合</summary>
+        /// <summary>
+        /// 根据排名查询指定缓存的count数量的值
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="count">数量</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
         /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
-        /// <param name="fromRank"></param>
-        /// <param name="toRank"></param>
         /// <returns></returns>
-        List<T> GetRangeFromSortedSetDesc<T>(string key, long fromRank, long toRank) where T : class, new();
+        List<T> SortedSetRangeByRank<T>(string key, long count = 1000, bool isDesc = true);
 
-        /// <summary>获取指定索引的集合</summary>
-        /// <param name="key"></param>
-        /// <param name="fromRank"></param>
-        /// <param name="toRank"></param>
+        /// <summary>
+        /// 根据缓存键获取从起始排名到终点排名的数据
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="fromRank">起始排名下标（包含）</param>
+        /// <param name="toRank">终点排名下标（包含）</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
         /// <returns></returns>
-        List<string> GetRangeFromSortedSet(string key, long fromRank, long toRank);
+        List<string> SortedSetRangeFrom(string key, long fromRank, long toRank, bool isDesc = true);
 
-        /// <summary>获取指定索引的集合</summary>
+        /// <summary>
+        /// 根据缓存键获取从起始排名到终点排名的数据
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="fromRank">起始排名下标（包含）</param>
+        /// <param name="toRank">终点排名下标（包含）</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
         /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
-        /// <param name="fromRank"></param>
-        /// <param name="toRank"></param>
         /// <returns></returns>
-        List<T> GetRangeFromSortedSet<T>(string key, long fromRank, long toRank) where T : class, new();
+        List<T> SortedSetRangeFrom<T>(string key, long fromRank, long toRank, bool isDesc = true);
 
-        /// <summary>判断是否存在项</summary>
+        /// <summary>
+        /// 查询指定缓存下的value是否存在
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">值</param>
         /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
         /// <returns></returns>
-        bool SortedSetExistItem<T>(string key, T value);
+        bool SortedSetExist<T>(string key, T value);
 
-        /// <summary>获取集合中的数量</summary>
-        /// <param name="key"></param>
+        /// <summary>
+        /// 得到指定缓存的SortSet长度
+        /// </summary>
+        /// <param name="key">缓存键</param>
         /// <returns></returns>
         long SortedSetLength(string key);
 
-        /// <summary>添加</summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
-        /// <param name="score"></param>
-        /// <returns></returns>
-        Task<bool> SortedSetAddAsync<T>(string key, T value, double score);
+        #endregion
 
-        /// <summary>删除</summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
+        #region 异步
+
+        /// <summary>
+        /// 设置SortSet类型的缓存键值对（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">缓存值</param>
+        /// <param name="score">分值</param>
+        /// <returns></returns>
+        Task<bool> SortedSetAsync(string key, string value, double score);
+
+        /// <summary>
+        /// 设置SortSet类型的缓存键值对（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">缓存值</param>
+        /// <param name="score">分值</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        Task<bool> SortedSetAsync<T>(string key, T value, double score);
+
+        /// <summary>
+        /// 删除指定的缓存键的value（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">值</param>
+        /// <returns></returns>
+        Task<bool> SortedSetRemoveAsync(string key, string value);
+
+        /// <summary>
+        /// 删除指定的缓存键的value（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">值</param>
+        /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         Task<bool> SortedSetRemoveAsync<T>(string key, T value);
 
-        /// <summary>获取全部</summary>
-        /// <param name="key"></param>
-        /// <param name="count"></param>
+        /// <summary>
+        /// 根据排名查询指定缓存的count数量的值（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="count">数量</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
         /// <returns></returns>
-        Task<List<string>> SortedSetRangeByRankAsync(string key, long count = 1000);
+        Task<List<string>> SortedSetRangeByRankAsync(string key, long count = 1000, bool isDesc = true);
 
-        /// <summary>获取全部</summary>
-        /// <param name="key"></param>
-        /// <param name="count"></param>
+        /// <summary>
+        /// 根据排名查询指定缓存的count数量的值（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="count">数量</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
+        /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        Task<List<T>> SortedSetRangeByRankAsync<T>(string key, long count = 1000) where T : class, new();
+        Task<List<T>> SortedSetRangeByRankAsync<T>(string key, long count = 1000, bool isDesc = true);
 
-        /// <summary>获取集合中的数量</summary>
-        /// <param name="key"></param>
+        /// <summary>
+        /// 根据缓存键获取从起始排名到终点排名的数据（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="fromRank">起始排名下标（包含）</param>
+        /// <param name="toRank">终点排名下标（包含）</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
+        /// <returns></returns>
+        Task<List<string>> SortedSetRangeFromAsync(string key, long fromRank, long toRank, bool isDesc = true);
+
+        /// <summary>
+        /// 根据缓存键获取从起始排名到终点排名的数据（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="fromRank">起始排名下标（包含）</param>
+        /// <param name="toRank">终点排名下标（包含）</param>
+        /// <param name="isDesc">是否降序，默认降序</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        Task<List<T>> SortedSetRangeFromAsync<T>(string key, long fromRank, long toRank, bool isDesc = true);
+
+        /// <summary>
+        /// 查询指定缓存下的value是否存在（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
+        /// <param name="value">值</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        Task<bool> SortedSetExistAsync<T>(string key, T value);
+
+        /// <summary>
+        /// 得到指定缓存的SortSet长度（异步）
+        /// </summary>
+        /// <param name="key">缓存键</param>
         /// <returns></returns>
         Task<long> SortedSetLengthAsync(string key);
 
-
-        // /// <summary>查找所有符合给定模式( pattern)的 key</summary>
-        // /// <param name="pattern">如：runoob*</param>
-        // /// <returns></returns>
-        // List<string> Keys(string pattern);
+        #endregion
     }
 }
